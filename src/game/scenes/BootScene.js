6f25379a21c77c705938d60271382f4b85cd292c
AdriaTM <48680115+AdriaTM@users.noreply.github.com>
2019-04-22 04:40:05 +0200

    Mapa

--- src/game/scenes/BootScene.js
+++ src/game/scenes/BootScene.js
@@ -1,7 +1,17 @@
 import {Scene} from 'phaser'
 import sky from '@/game/assets/prova.png';
 import bomb from '@/game/assets/bomb.png';
-import barrera from '@/game/assets/barrera.png';
+import barrera from '@/game/assets/barrera2.png';
+import barrera2 from '@/game/assets/barrera3.png';
+import barrera3 from '@/game/assets/barrera4.png';
+import barrera_colors_taronja from '@/game/assets/barrera-colors.png';
+import barrera_colors_blau from '@/game/assets/barrera-colors-1.png';
+import creu from '@/game/assets/creuv.png';
+import creub from '@/game/assets/creub.png';
+import creut from '@/game/assets/creut.png';
+import forma from '@/game/assets/forma.png';
+
+
 
 export default class BootScene extends Scene {
     constructor() {
@@ -12,6 +22,14 @@ export default class BootScene extends Scene {
         this.load.image('sky', sky);
         this.load.image('bomb', bomb);
         this.load.image('barrera', barrera);
+        this.load.image('barrera2', barrera2);
+        this.load.image('barrera3', barrera3);
+        this.load.image('barrera_colors_taronja', barrera_colors_taronja);
+        this.load.image('barrera_colors_blau', barrera_colors_blau);
+        this.load.image('creu', creu);
+        this.load.image('creub', creub);
+        this.load.image('creut', creut);
+        this.load.image('forma', forma);
         // this.load.audio('thud', ['assets/thud.mp3', 'assets/thud.ogg'])
     }
 
--- src/game/scenes/PlayScene.js
+++ src/game/scenes/PlayScene.js
@@ -8,6 +8,19 @@ var velocitat = 1;
 var rotacio = 0.05;
 var barrera1;
 var barrera2;
+var barrera3;
+var barrera4;
+var barrera5;
+var barrera6;
+var barrera_colors1;
+var barrera_colors2;
+var creu;
+var creu_blau;
+var creu_taronja;
+var creu1;
+var creu_blau1;
+var creu_taronja1;
+var forma;
 
 export default class PlayScene extends Scene {
   constructor () {
@@ -17,23 +30,38 @@ export default class PlayScene extends Scene {
   create () {
     this.add.image(600, 300, 'sky');
 
-    barrera1=this.physics.add.staticImage(600,10,'barrera');
-    barrera2=this.physics.add.staticImage(600,590,'barrera');
+    barrera1=this.physics.add.staticImage(600, 590,'barrera');
+    barrera2=this.physics.add.staticImage(600,12,'barrera');
+    barrera3=this.physics.add.staticImage(1190, 300, 'barrera2');
+    barrera4=this.physics.add.staticImage(14, 300, 'barrera2');
+    barrera_colors1=this.physics.add.staticImage(995, 155, 'barrera_colors_blau');
+    barrera_colors2=this.physics.add.staticImage(205, 445, 'barrera_colors_taronja');
+    creu=this.physics.add.staticImage(348, 157, 'creu');
+    creu1=this.physics.add.staticImage(852, 444, 'creu');
+    creu_blau=this.physics.add.staticImage(900, 444, 'creub');
+    creu_blau1=this.physics.add.staticImage(805, 444, 'creub');
+    creu_taronja=this.physics.add.staticImage(396, 157, 'creut');
+    creu_taronja1=this.physics.add.staticImage(300, 157, 'creut');
+    forma= this.physics.add.staticImage(600, 300, 'forma');
+
+
 
     player1 = this.physics.add.image(100, 100, 'bomb');
-
     player1.body.setVelocity(100, 200).setBounce(1, 1).setCollideWorldBounds(true);
 
-    /*
-    barrera.create(600,10,'barrera');
-    barrera.create(600,590,'barrera');
-    barrera.create(600,300,'barrera');
-     */
+    player2 = this.physics.add.image(780, 580, 'bomb');
+    player2.body.setVelocity(100, 200).setBounce(1, 1).setCollideWorldBounds(true);
+
 	// enable worldbounds collision event
-    /*1
+/*
+    player1 = this.physics.add.sprite(100, 100, 'bomb');
+    player1.setCollideWorldBounds(true);
+    player1.body.onWorldBounds = true; // enable worldbounds collision event
+    player1.rotation = Math.PI;
+
 	player2 = this.physics.add.sprite(780, 580, 'bomb');
     player2.setCollideWorldBounds(true);
-    player2.body.onWorldBounds = true; // enable worldbounds collision event
+    player2.body.onWorldBounds = true; //enable worldbounds collision event
     player2.rotation = Math.PI;
 */
 
@@ -55,7 +83,20 @@ export default class PlayScene extends Scene {
   }
 
   update () {
-    this.physics.world.collide(player1, [barrera1, barrera2]);
+    this.physics.world.collide([player1,player2], [barrera1, barrera2, barrera3, barrera4, barrera5, barrera6, barrera_colors1, barrera_colors2, creu, creu_taronja, creu_blau,
+      creu1, creu_taronja1, creu_blau1, forma], function(){
+            console.log("1");
+      });
+
+    forma.angle += 0.5;
+    /*
+    creu.angle += 0.5;
+    creu1.angle += 0.5;
+    creu_blau.angle += 0.5;
+    creu_blau1.angle += 0.5;
+    creu_taronja.angle += 0.5;
+    creu_taronja1.angle += 0.5;
+
     /*
   // CONTROLS NO AUTOMATICS
 	// CONTROLS player1
@@ -107,7 +148,7 @@ export default class PlayScene extends Scene {
     {
         player1.rotation -= rotacio;
     }
-    /*
+
     // CONTROLS player2
     if (keys.a.isDown)
     {
@@ -117,13 +158,13 @@ export default class PlayScene extends Scene {
     {
         player2.rotation -= rotacio;
     }
-*/
+
     player1.x += velocitat * Math.sin(player1.rotation);
     player1.y += velocitat * Math.cos(player1.rotation);
-/*
+
     player2.x += velocitat * Math.sin(player2.rotation);
     player2.y += velocitat * Math.cos(player2.rotation);
-*/
+
 
 
   }
